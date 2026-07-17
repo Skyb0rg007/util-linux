f32dcc2f8c25a3ae559d883c56dfc6383428fe45
Skye Soss <skye@soss.website>
2026-07-17 18:07:54 -0500

    setpriv: add Landlock ABI-compatibility infrastructure
    Define Landlock macros that are not available with the current version
    of the <linux/landlock.h> file. Also define a local version of the
    landlock_ruleset_attr struct to use fields not in the header. The set of
    Landlock rules that are recognized by setpriv is now exclusively
    determined by the util-linux version.
    
    Signed-off-by: Skye Soss <skye@soss.website>

--- sys-utils/setpriv-landlock.c
+++ sys-utils/setpriv-landlock.c
@@ -11,7 +11,6 @@
 
 #include <sys/prctl.h>
 #include <sys/syscall.h>
-#include <linux/landlock.h>
 
 #include "setpriv-landlock.h"
 
@@ -19,6 +18,74 @@
 #include "xalloc.h"
 #include "nls.h"
 #include "c.h"
+
+/*
+ * <linux/landlock.h> is deliberately not included: its contents (which
+ * macros, the rule-type enum members, and the ruleset/rule attr struct
+ * fields exist) vary by kernel version, so patching around a possibly-old
+ * copy with #ifndef would only cover whatever gaps the systems seen so far
+ * happened to have. setpriv only ever reaches Landlock through the raw
+ * syscalls below, so it vendors every definition it needs itself instead,
+ * matching the stable kernel ABI.
+ */
+
+#define LANDLOCK_CREATE_RULESET_VERSION		(1U << 0)
+
+enum landlock_rule_type {
+	LANDLOCK_RULE_PATH_BENEATH = 1,
+	LANDLOCK_RULE_NET_PORT = 2,
+};
+
+struct landlock_ruleset_attr {
+	uint64_t handled_access_fs;
+	uint64_t handled_access_net;
+	uint64_t scoped;
+	uint64_t quiet_access_fs;
+	uint64_t quiet_access_net;
+	uint64_t quiet_scoped;
+};
+
+struct landlock_path_beneath_attr {
+	uint64_t allowed_access;
+	int32_t parent_fd;
+} __attribute__((packed));
+
+struct landlock_net_port_attr {
+	uint64_t allowed_access;
+	uint64_t port;
+};
+
+#define LANDLOCK_ACCESS_FS_EXECUTE			(1ULL << 0)
+#define LANDLOCK_ACCESS_FS_WRITE_FILE			(1ULL << 1)
+#define LANDLOCK_ACCESS_FS_READ_FILE			(1ULL << 2)
+#define LANDLOCK_ACCESS_FS_READ_DIR			(1ULL << 3)
+#define LANDLOCK_ACCESS_FS_REMOVE_DIR			(1ULL << 4)
+#define LANDLOCK_ACCESS_FS_REMOVE_FILE			(1ULL << 5)
+#define LANDLOCK_ACCESS_FS_MAKE_CHAR			(1ULL << 6)
+#define LANDLOCK_ACCESS_FS_MAKE_DIR			(1ULL << 7)
+#define LANDLOCK_ACCESS_FS_MAKE_REG			(1ULL << 8)
+#define LANDLOCK_ACCESS_FS_MAKE_SOCK			(1ULL << 9)
+#define LANDLOCK_ACCESS_FS_MAKE_FIFO			(1ULL << 10)
+#define LANDLOCK_ACCESS_FS_MAKE_BLOCK			(1ULL << 11)
+#define LANDLOCK_ACCESS_FS_MAKE_SYM			(1ULL << 12)
+#define LANDLOCK_ACCESS_FS_REFER			(1ULL << 13)
+#define LANDLOCK_ACCESS_FS_TRUNCATE			(1ULL << 14)
+#define LANDLOCK_ACCESS_FS_IOCTL_DEV			(1ULL << 15)
+#define LANDLOCK_ACCESS_FS_RESOLVE_UNIX			(1ULL << 16)
+
+#define LANDLOCK_ACCESS_NET_BIND_TCP			(1ULL << 0)
+#define LANDLOCK_ACCESS_NET_CONNECT_TCP			(1ULL << 1)
+#define LANDLOCK_ACCESS_NET_BIND_UDP			(1ULL << 2)
+#define LANDLOCK_ACCESS_NET_CONNECT_SEND_UDP		(1ULL << 3)
+
+#define LANDLOCK_SCOPE_ABSTRACT_UNIX_SOCKET		(1ULL << 0)
+#define LANDLOCK_SCOPE_SIGNAL				(1ULL << 1)
+
+#define LANDLOCK_RESTRICT_SELF_LOG_SAME_EXEC_OFF	(1U << 0)
+#define LANDLOCK_RESTRICT_SELF_LOG_NEW_EXEC_ON		(1U << 1)
+#define LANDLOCK_RESTRICT_SELF_LOG_SUBDOMAINS_OFF	(1U << 2)
+
+#define LANDLOCK_ADD_RULE_QUIET			(1U << 0)
 
 #ifndef HAVE_LANDLOCK_CREATE_RULESET
 static inline int landlock_create_ruleset(
@@ -56,11 +123,13 @@ struct landlock_rule_entry {
 	};
 };
 
-static const struct {
+struct landlock_access_entry {
 	unsigned long long value;
 	const char *type;
 	const char *help;
-} landlock_access_fs[] = {
+};
+
+static const struct landlock_access_entry landlock_access_fs[] = {
 	{ LANDLOCK_ACCESS_FS_EXECUTE,     "execute",     N_("execute a file") },
 	{ LANDLOCK_ACCESS_FS_WRITE_FILE,  "write-file",  N_("open a file with write access") },
 	{ LANDLOCK_ACCESS_FS_READ_FILE,   "read-file",   N_("open a file with read access") },
@@ -74,15 +143,10 @@ static const struct {
 	{ LANDLOCK_ACCESS_FS_MAKE_FIFO,   "make-fifo",   N_("create (or rename or link) a named pipe") },
 	{ LANDLOCK_ACCESS_FS_MAKE_BLOCK,  "make-block",  N_("create (or rename or link) a block device") },
 	{ LANDLOCK_ACCESS_FS_MAKE_SYM,    "make-sym",    N_("create (or rename or link) a symbolic link") },
-#ifdef LANDLOCK_ACCESS_FS_REFER
 	{ LANDLOCK_ACCESS_FS_REFER,       "refer",       N_("link or rename a file from or to a different directory") },
-#endif
-#ifdef LANDLOCK_ACCESS_FS_TRUNCATE
 	{ LANDLOCK_ACCESS_FS_TRUNCATE,    "truncate",    N_("truncate a file with truncate(2)") },
-#endif
-#ifdef LANDLOCK_ACCESS_FS_IOCTL_DEV
 	{ LANDLOCK_ACCESS_FS_IOCTL_DEV,   "ioctl-dev",   N_("invoke ioctl(2) on an opened character or block device") },
-#endif
+	{ LANDLOCK_ACCESS_FS_RESOLVE_UNIX, "resolve-unix", N_("connect(2) or bind(2) a pathname UNIX domain socket") },
 };
 
 static long landlock_access_to_mask(const char *str, size_t len)
